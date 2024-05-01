fa61c89a0e286f516d5fddbf52ae59e9dd0e5a9b
Alexey Morozov <alexeymorozov.dev@gmail.com>
2024-05-01 19:52:02 +0300

    Added chat index page

---
+++ resources/js/Layouts/Main.vue
@@ -0,0 +1,27 @@
+<script>
+import { Link } from '@inertiajs/vue3'
+
+export default {
+    components: {
+        Link
+    }
+}
+
+</script>
+
+<template>
+    <div class="w-3/4 p-4 m-auto">
+        <div>
+            <div>
+                <Link :href="route('chats.index')">Chats</Link>
+            </div>
+        </div>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Chat/Index.vue
@@ -0,0 +1,25 @@
+<script>
+import Main from "@/Layouts/Main.vue"
+
+export default {
+    name: 'Index',
+
+    layout: Main
+}
+</script>
+
+<template>
+    <div class="flex">
+        <div class="p-2 w-1/2 border border-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda corporis culpa cumque deserunt doloremque eos eveniet iusto numquam, perferendis quae quam quos repellendus temporibus tenetur totam velit, voluptas. Repellendus?
+        </div>
+
+        <div class="p-2 w-1/2 border border-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda corporis culpa cumque deserunt doloremque eos eveniet iusto numquam, perferendis quae quam quos repellendus temporibus tenetur totam velit, voluptas. Repellendus?
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
