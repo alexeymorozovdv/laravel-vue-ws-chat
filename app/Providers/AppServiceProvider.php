ab84a3dc908252b7502c7bfdc97837352f6c3f5e
Alexey Morozov <alexeymorozov.dev@gmail.com>
2024-05-01 19:11:52 +0300

    Added helpers to app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Http\Kernel;
+use Carbon\CarbonInterval;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Connection;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading(!app()->isProduction());
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+        DB::whenQueryingForLongerThan(500, function (Connection $connection) {
+            logger()
+                ->channel('telegram')
+                ->info('Slow query: ' . $connection->query()->toSql());
+        });
+
+        $kernel = app(Kernel::class);
+        $kernel->whenRequestLifecycleIsLongerThan(
+            CarbonInterval::seconds(4),
+            function () {
+                logger()
+                    ->channel('telegram')
+                    ->info('Slow request: ' . request()->url());
+            }
+        );
     }
 }
